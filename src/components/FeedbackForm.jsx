448b362c6da5f97a357bf05d5d4a38c142d27976
sourabhpatole <sourabhptole@outlook.com>
2022-05-11 11:26:08 +0530

    initital

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,29 +1,41 @@
-import React, { useContext, useState } from "react";
-import FeedbackContext from "../context/FeedbackContext";
+import { useState, useContext, useEffect } from "react";
 import RatingSelect from "./RatingSelect";
-import Button from "./shared/Button";
 import Card from "./shared/Card";
+import Button from "./shared/Button";
+import FeedbackContext from "../context/FeedbackContext";
 
-const FeedbackForm = () => {
+function FeedbackForm() {
   const [text, setText] = useState("");
-  const [btnDisabled, setBtnDisabled] = useState(true);
   const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
 
-  const { addFeedback } = useContext(FeedbackContext);
+  const { addFeedback, feedbackEdit, updateFeedback } =
+    useContext(FeedbackContext);
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false);
+      setText(feedbackEdit.item.text);
+      setRating(feedbackEdit.item.rating);
+    }
+  }, [feedbackEdit]);
+
   const handleTextChange = (e) => {
     if (text === "") {
       setBtnDisabled(true);
       setMessage(null);
     } else if (text !== "" && text.trim().length <= 10) {
-      setBtnDisabled(true);
       setMessage("Text must be at least 10 characters");
+      setBtnDisabled(true);
     } else {
       setMessage(null);
       setBtnDisabled(false);
     }
+
     setText(e.target.value);
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (text.trim().length > 10) {
@@ -31,10 +43,17 @@ const FeedbackForm = () => {
         text,
         rating,
       };
-      addFeedback(newFeedback);
+
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback);
+      } else {
+        addFeedback(newFeedback);
+      }
+
       setText("");
     }
   };
+
   return (
     <Card>
       <form onSubmit={handleSubmit}>
@@ -44,17 +63,18 @@ const FeedbackForm = () => {
           <input
             onChange={handleTextChange}
             type="text"
-            placeholder="write a review"
+            placeholder="Write a review"
             value={text}
           />
           <Button type="submit" isDisabled={btnDisabled}>
             Send
           </Button>
         </div>
+
         {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
-};
+}
 
 export default FeedbackForm;
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,5 @@
-import { createContext, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { createContext, useState } from "react";
 
 const FeedbackContext = createContext();
 
@@ -7,29 +7,67 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: "This item is from context",
+      text: "This is feedback item 1",
       rating: 10,
     },
+    {
+      id: 2,
+      text: "This is feedback item 2",
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: "This is feedback item 3",
+      rating: 7,
+    },
   ]);
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  // Add feedback
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  // Delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+
+  // Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
   };
+
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
+        editFeedback,
+        updateFeedback,
       }}
     >
       {children}
     </FeedbackContext.Provider>
   );
 };
+
 export default FeedbackContext;
